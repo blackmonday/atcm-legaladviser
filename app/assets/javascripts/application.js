f4acde72633b728b42dd568fce0b8dccd9bc3d15
Larry Miles <lmiles@lundinoir.com>
2018-03-22 14:41:31 +0000

    Thursday commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,136 @@
+/* global $ */
+/* global GOVUK */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+  // with role="button" when the space key is pressed.
+  GOVUK.shimLinksWithButtonRole.init()
+
+  // Show and hide toggled content
+  // Where .multiple-choice uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+    
+    
+    
+    
+    
+    $( "#progress-bar-1" ).animate({
+        width: "100%"
+    }, 5000, function() {
+        document.getElementById("progressBar1").style.display = "none";
+        document.getElementById("uploadDate1").style.display = "table-cell";
+        document.getElementById("uploadTime1").style.display = "table-cell";
+        document.getElementById("row1cancel").style.display = "none";
+        document.getElementById("row1remove").style.display = "table-cell";
+
+    });
+
+    $( "#progress-bar-2" ).animate({
+        width: "100%"
+    }, 1500, function() {
+        document.getElementById("progressBar2").style.display = "none";
+        document.getElementById("uploadDate2").style.display = "table-cell";
+        document.getElementById("uploadTime2").style.display = "table-cell";
+        document.getElementById("row2cancel").style.display = "none";
+        document.getElementById("row2remove").style.display = "table-cell";
+    });
+
+    $( "#progress-bar-3" ).animate({
+        width: "100%",
+    }, 8000, function() {
+        document.getElementById("progressBar3").style.display = "none";
+        document.getElementById("uploadDate3").style.display = "table-cell";
+        document.getElementById("uploadTime3").style.display = "table-cell";
+        document.getElementById("row3cancel").style.display = "none";
+        document.getElementById("row3remove").style.display = "table-cell";
+    });
+    
+})
+
+
+
+/* TABS PATTERN */
+function tabGroup(tableObj, which) {
+
+    tabA = 'TAB-ONE';
+    tabB = 'TAB-TWO';
+    tabC = 'TAB-THREE';
+
+    if(which == tabA) {
+        document.getElementById("tabA").className = "tab-on";
+        document.getElementById("tabB").className = "tab";
+        document.getElementById("tabOneContent").style.display = "block";
+        document.getElementById("tabTwoContent").style.display = "none";
+    } else if(which == tabB) {
+        document.getElementById("tabA").className = "tab";
+        document.getElementById("tabB").className = "tab-on";
+        document.getElementById("tabOneContent").style.display = "none";
+        document.getElementById("tabTwoContent").style.display = "block";
+    } 
+}
+
+function tabGroup2(tableObj, which) {
+
+    tabA2 = 'TAB-ONE-2';
+    tabB2 = 'TAB-TWO-2';
+    tabC2 = 'TAB-THREE-2';
+
+    if(which == tabA2) {
+        document.getElementById("tabA2").className = "tab-on";
+        document.getElementById("tabB2").className = "tab";
+        document.getElementById("tabOneContent2").style.display = "block";
+        document.getElementById("tabTwoContent2").style.display = "none";
+    } else if(which == tabB2) {
+        document.getElementById("tabA2").className = "tab";
+        document.getElementById("tabB2").className = "tab-on";
+        document.getElementById("tabOneContent2").style.display = "none";
+        document.getElementById("tabTwoContent2").style.display = "block";
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/* FILE 1 */
+function overlay_file_1_on() {
+    document.getElementById("overlay-file-1").style.display = "block";
+}
+function overlay_file_1_off() {
+    document.getElementById("overlay-file-1").style.display = "none";
+}
+function removeFile1() {
+    document.getElementById("row1").style.display = "none";
+}
+
+
+/* UPLOAD DOCUMENTS & UPLOADING DOCUMENTS */
+function on2() {
+    document.getElementById("overlay2").style.display = "block";
+}
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+function off() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay2").style.display = "none";
+}
+function removeRow1() {
+    /*document.getElementById("row1").style.display = "none";*/
+    document.getElementById("noFilesAdded").style.display = "block";
+    document.getElementById("filesAdded").style.display = "none";
+}
---
+++ app/routes.js
@@ -0,0 +1,135 @@
+var express = require('express')
+var router = express.Router()
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+// add your routes here
+
+module.exports = router
+
+// *****
+// INDEX
+router.post('/', function (req, res) {
+    
+    var prosecutor = req.session.data['prosecutor-group']
+
+    if (prosecutor == "DVLA (with back duty)") {
+        req.session.data['penalty-or-fine'] = "Penalty"
+        req.session.data['defendents-first-name'] = "Camilla "
+        req.session.data['defendents-last-name'] = "JONES"
+        req.session.data['defendents-age'] = "4 April 1970 (46 years old)"
+        req.session.data['defendents-address-line-1'] = "6 Blessington Road"
+        req.session.data['defendents-address-line-2'] = "Lewisham"
+        req.session.data['defendents-town-or-city'] = "London"
+        req.session.data['defendents-postcode'] = "SE13 5EB"       
+        req.session.data['defendents-previous-convictions'] = "2"
+        req.session.data['defendents-income'] = "£142.92 weekly (actual)"
+        req.session.data['defendents-claiming-benefits'] = "No"
+        req.session.data['defendents-employment-status'] = "Employed (full or part-time)"
+        req.session.data['defendents-plea'] = "Pleaded guilty SJP"
+        req.session.data['case-status'] = "Plea received - ready for decision"
+        req.session.data['prosecutor'] = "DVLA"
+        req.session.data['notice-served'] = "30 April 2018"
+        req.session.data['dates-to-avoid'] = "18 June 2018"
+        req.session.data['offence-type'] = "Keep a vehicle without a vehicle license"
+        req.session.data['offence-description'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        req.session.data['statement-of-facts'] = "Eiusmod tempor orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        req.session.data['back-duty-description'] = "2 months unpaid duty"
+        req.session.data['offence-banding-header'] = "Check penalty, back duty and compensation"
+        req.session.data['guilty-plea-taken-into-account'] = "Yes"
+        req.session.data['verdict'] = "Proved SJP"
+        req.session.data['payment-method'] = "Pay directly to court"
+        req.session.data['deduct-from-benefits-reason'] = "No information from the defendant"
+        req.session.data['how-should-defendent-pay'] = "Lump sum amount"
+
+        
+        res.redirect('/legaladviser/dashboard')
+        
+    } else if (prosecutor == "DVLA (no back duty)") {
+        req.session.data['penalty-or-fine'] = "Fine"
+        req.session.data['defendents-first-name'] = "Billy"
+        req.session.data['defendents-last-name'] = "SMITH"
+        res.redirect('/legaladviser/dashboard')
+    }
+
+})
+
+// *****************
+// CASE DETAILS PAGE
+router.post('/legaladviser/case-details-page', function (req, res) {
+    
+    req.session.data['decision'] = req.session.data['make-a-decision-group']
+    res.redirect('/legaladviser/confirm-outcome')
+    
+})
+
+// ***************
+// CONFIRM OUTCOME
+router.post('/legaladviser/confirm-outcome', function (req, res) {
+    
+    var fine = Number(req.session.data['penalty-or-fine-amount'])
+    var backduty = Number(req.session.data['back-duty-amount'])
+    var compensation = Number(req.session.data['compensation-amount'])
+    var costs = Number(req.session.data['costs'])
+    
+    req.session.data['total-payment'] = fine+backduty+compensation+costs;
+    
+    res.redirect('/legaladviser/payment-method')
+    
+})
+
+// **************
+// PAYMENT METHOD
+router.post('/legaladviser/payment-method', function (req, res) {
+    
+    res.redirect('/legaladviser/pay-direct-to-court')
+    
+})
+
+// *******************
+// PAY DIRECT TO COURT
+router.post('/legaladviser/pay-direct-to-court', function (req, res) {
+    
+    res.redirect('/legaladviser/check-your-decision')
+    
+})
+
+// ********************
+// CHECK YOUR DECISIONS
+router.post('/legaladviser/check-your-decision', function (req, res) {
+ 
+    req.session.data['case-status'] = "Completed"
+    req.session.data['defendents-plea'] = req.session.data['verdict']
+    
+    req.session.data['payment-terms'] = "to be paid as a lump sum in 28 days"
+
+    res.redirect('/legaladviser/case-details-page')
+    
+})
+
+
+
+
+
+
+
+
+
+
+// ******************************
+// ADD OR CHANGE PERSONAL DETAILS
+router.post('/legaladviser/add-or-change-personal-details', function (req, res) {
+
+    res.redirect('/legaladviser/add-or-change-income')
+    
+})
+// ********************
+// ADD OR CHANGE INCOME
+router.post('/legaladviser/add-or-change-income', function (req, res) {
+
+    res.redirect('/legaladviser/upload-documents')
+    
+})
